e2dfb96bde1167d5c5c6947e0f6065e0a190f3d6
2268849815 <2268849815@qq.com>
2020-05-30 17:09:21 +0800

    js

---
+++ js面试题/6.js
@@ -0,0 +1,22 @@
+// var arr = [1];
+// arr[10000] = 1;
+
+
+// function a() {
+//   console.time();
+//   for (var i = 0; i < arr.length; i++) {
+//     console.log(1)
+//   }
+//   console.timeEnd();
+// }
+
+
+// a();
+// console.log(arr)
+// function b() {
+//   console.time();
+//   arr.forEach(item => { console.log(2) })
+//   console.timeEnd();
+// }
+// b()
+
