555c77939d95c9d82e08ec6089a0bdec53f1896f
2268849815 <2268849815@qq.com>
2020-06-10 00:00:49 +0800

    js

---
+++ js面试题/8.js
@@ -0,0 +1,23 @@
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+const lydia = new Person("Lydia", "Hallie");
+const sarah = Person("Sarah", "Smith");
+
+console.log(lydia);
+console.log(sarah);
+
+
+
+function getPersonInfo(one, two, three) {
+  console.log(one);
+  console.log(two);
+  console.log(three);
+}
+
+const person = "Lydia";
+const age = 21;
+
+getPersonInfo`${person} is ${age} years old`;
