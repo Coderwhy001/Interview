4d9977271c9a9e8d0d428a41d6eb786b88e2ce7e
2268849815 <2268849815@qq.com>
2020-06-14 22:47:37 +0800

    bind

--- call_apply_bind/bind.js
+++ call_apply_bind/bind.js
@@ -1,3 +1,20 @@
+Function.prototype.myBind = function(objThis, ...params) { // 先传一部分参数
+  const thisFn = this; //  存储源函数的this
+  let fToBind = function(...secondParams) { //  最后还是要执行的
+    const isNew = this instanceof fToBind // new 调用
+    const context = isNew ? this : Object(objThis) ; // this 覆盖为上下文对象
+    return thisFn.call(context, ...params, ...secondParams); // 
+  }
+
+
+  if (thisFn.prototype) {
+    fToBind.prototype = Object.create(thisFn.prototype);
+  }
+  
+  return fToBind; 
+}
+
+
 Function.prototype.myBind = function (context, ...args) {
   const fn = this
   args = args ? args : []
