148924833656859b28b56cc786f612ec77d2ce90
2268849815 <2268849815@qq.com>
2020-06-16 22:16:27 +0800

    promise

--- js面试题/promise/all.js
+++ js面试题/promise/all.js
@@ -23,19 +23,34 @@
 // })
 
 
-let wake = (time) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`${time / 1000}秒后醒来`)
-    }, time)
-  })
+// let wake = (time) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(`${time / 1000}秒后醒来`)
+//     }, time)
+//   })
+// }
+
+// let p1 = wake(3000)
+// let p2 = wake(2000)
+
+// Promise.all([p1, p2]).then((result) => {
+//   console.log(result)       // [ '3秒后醒来', '2秒后醒来' ]
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+
+
+var b=2
+function s1() {
+  function s2(){
+    b++
+    console.log(b)
+    var b = 3
+  }
+  s2()
 }
+s1()
 
-let p1 = wake(3000)
-let p2 = wake(2000)
-
-Promise.all([p1, p2]).then((result) => {
-  console.log(result)       // [ '3秒后醒来', '2秒后醒来' ]
-}).catch((error) => {
-  console.log(error)
-})
+console.log(undefined+1)
--- js面试题/promise/readme.md
+++ js面试题/promise/readme.md
@@ -3,3 +3,5 @@ Promise.all可以将多个Promise实例包装成一个新的Promise实例。 同
 - 需要特别注意的是，Promise.all获得的成功结果的数组里面的数据顺序和Promise.all接收到的数组顺序是一致的，即p1的结果在前，即便p1的结果获取的比p2要晚。这带来了一个绝大的好处：在前端开发请求数据的过程中，偶尔会遇到发送多个请求并根据请求顺序获取和使用数据的场景，使用Promise.all毫无疑问可以解决这个问题。
 # Promise.race的使用
 顾名思义，Promse.race就是赛跑的意思，意思就是说，Promise.race([p1, p2, p3])里面哪个结果获得的快，就返回那个结果，不管结果本身是成功状态还是失败状态。
+# Promise和async await的区别
+1. 函数前面多了一个async关键字。 await只能用在async定义的函数内。 async函数会隐式的返回一个promise， 该promise的resolve值就是函数return的值
